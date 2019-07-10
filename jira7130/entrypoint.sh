62f8f9406ddf5ea41a18aed31d09b8074ab4e264
root <root@vmd31384.contaboserver.net>
2019-07-10 06:41:44 +0200

    add jira 7.13.0 with postgres

---
+++ jira7130/entrypoint.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+set -euo pipefail
+
+# Setup Catalina Opts
+: ${CATALINA_CONNECTOR_PROXYNAME:=}
+: ${CATALINA_CONNECTOR_PROXYPORT:=}
+: ${CATALINA_CONNECTOR_SCHEME:=http}
+: ${CATALINA_CONNECTOR_SECURE:=false}
+: ${CATALINA_CONTEXT_PATH:=}
+
+: ${CATALINA_OPTS:=}
+
+: ${JAVA_OPTS:=}
+
+CATALINA_OPTS="${CATALINA_OPTS} -DcatalinaConnectorProxyName=${CATALINA_CONNECTOR_PROXYNAME}"
+CATALINA_OPTS="${CATALINA_OPTS} -DcatalinaConnectorProxyPort=${CATALINA_CONNECTOR_PROXYPORT}"
+CATALINA_OPTS="${CATALINA_OPTS} -DcatalinaConnectorScheme=${CATALINA_CONNECTOR_SCHEME}"
+CATALINA_OPTS="${CATALINA_OPTS} -DcatalinaConnectorSecure=${CATALINA_CONNECTOR_SECURE}"
+CATALINA_OPTS="${CATALINA_OPTS} -DcatalinaContextPath=${CATALINA_CONTEXT_PATH}"
+
+export JAVA_OPTS="${JAVA_OPTS} ${CATALINA_OPTS}"
+
+cp /dbconfig.xml ${JIRA_HOME} \
+&& sed -i -e "s/EXTDB_HOST/${EXTDB_HOST}/" ${JIRA_HOME}/dbconfig.xml \
+&& sed -i -e "s/EXTDB_NAME/${EXTDB_NAME}/" ${JIRA_HOME}/dbconfig.xml \
+&& sed -i -e "s/EXTDB_USER/${EXTDB_USER}/" ${JIRA_HOME}/dbconfig.xml \
+&& sed -i -e "s/EXTDB_PASSWORD/${EXTDB_PASSWORD}/" ${JIRA_HOME}/dbconfig.xml
+
+# Start JIRA as the correct user
+if [ "${UID}" -eq 0 ]; then
+    echo "User is currently root. Will change directory ownership to ${RUN_USER}:${RUN_GROUP}, then downgrade permission to ${RUN_USER}"
+    PERMISSIONS_SIGNATURE=$(stat -c "%u:%U:%a" "${JIRA_HOME}")
+    EXPECTED_PERMISSIONS=$(id -u ${RUN_USER}):${RUN_USER}:700
+    if [ "${PERMISSIONS_SIGNATURE}" != "${EXPECTED_PERMISSIONS}" ]; then
+        chmod -R 700 "${JIRA_HOME}" &&
+            chown -R "${RUN_USER}:${RUN_GROUP}" "${JIRA_HOME}"
+    fi
+    # Now drop privileges
+    exec su -s /bin/bash "${RUN_USER}" -c "$JIRA_INSTALL_DIR/bin/start-jira.sh $@"
+else
+    exec "$JIRA_INSTALL_DIR/bin/start-jira.sh" "$@"
+fi
